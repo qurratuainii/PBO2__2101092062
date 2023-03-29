6bcf578309deafa4d0fc47800d88ed78796d03cf
qurratuainii <aqurratu09des@gmail.com>
2023-03-29 12:25:32 +0700

    Update Koneksi.java

--- src/aini/Dao/Koneksi.java
+++ src/aini/Dao/Koneksi.java
@@ -21,4 +21,8 @@ public class Koneksi {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(url, username, password);
     }
+
+    Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
