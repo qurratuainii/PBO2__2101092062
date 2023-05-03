c3eea5fcf8a98a4e139becd6ed6380020c8ef1ca
qurratuainii <aqurratu09des@gmail.com>
2023-05-03 20:21:36 +0700

    Create Buku.java

---
+++ src/aini/Model/Buku.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aini.Model;
+
+/**
+ *
+ * @author USER
+ */
+public class Buku {
+    private String kodebuku;
+    private String judul;
+    private String pengarang;
+    private String penerbit;
+    
+    public Buku(){
+        
+    }
+    
+    public Buku(String kodebuku, String judul, String pengarang, String penerbit){
+        this.kodebuku = kodebuku;
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.penerbit = penerbit;
+    }
+
+    public void setKodebuku(String kodebuku) {
+        this.kodebuku = kodebuku;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+    
+    public String getKodebuku() {
+        return kodebuku;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public String getPenerbit() {
+        return penerbit;
+    }
+    
+    
+}
